d5bdb777e4b3cdab1ae9d9d8ad319a75fa10f891
rizkyananda <rizkyananda007@gmail.com>
2022-12-16 15:56:45 +0800

    Add folder for Detail Page

---
+++ Project-DotNet-Dojo/Pages/Details/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_DotNet_Dojo.Pages.Details
+{
+    public class IndexModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
